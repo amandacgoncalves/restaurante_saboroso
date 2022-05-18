a8748097103c6d9a9e19f2820da84397cd73e915
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-18 11:20:53 -0300

    consulting mysql

--- saboroso/inc/admin.js
+++ saboroso/inc/admin.js
@@ -1,4 +1,41 @@
+const Connection = require("mysql2/typings/mysql/lib/Connection")
+
 module.exports = {
+
+    dashboard(){
+
+        return new Promise((resolve, reject)=>{
+
+            conn.query(`
+                SELECT
+        (SELECT 
+                COUNT(*)
+            FROM
+                tb_contacts) AS nrcontacts,
+        (SELECT 
+                COUNT(*)
+            FROM
+                tb_menus) AS nrmenus,
+        (SELECT 
+                COUNT(*)
+            FROM
+                tb_reservations) AS nrreservations,
+        (SELECT 
+                COUNT(*)
+            FROM
+                tb_users) AS nrusers;
+                
+            `, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results[0]);
+                }
+            });//conn query
+
+        });//new promise
+
+    },
 
     getParams(req, params){
 
