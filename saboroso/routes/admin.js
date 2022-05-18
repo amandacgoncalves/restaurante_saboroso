c87f225ec9afdf595e945a330ef342d06d75c1d7
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-18 11:32:55 -0300

    administrating food menu

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 var express = require ('express');
 const users = require('../inc/users');
 var users = require('./../inc/users')
-var admin = require('./../inc/admin')
+var admin = require('./../inc/admin');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 router.use(function (req, res, next){
@@ -83,7 +84,13 @@ router.get('/emails', function(req, res, next) {
 
 router.get('/menus', function(req, res, next) {
 
-    res.render('admin/menus', admin.getParams(req));
+    menus.getMenus().then(data =>{
+
+        res.render('admin/menus', admin.getParams(req, {
+            data
+        })); 
+
+    });//menu get menus
 
 });//router get menus
 
