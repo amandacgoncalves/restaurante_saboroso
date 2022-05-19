487e06050dd8ff35aaa2a50d792f9fd941070e7b
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-05-19 09:27:15 -0300

    changing files via post

--- saboroso/inc/menus.js
+++ saboroso/inc/menus.js
@@ -25,21 +25,54 @@ module.exports = {
 
     },
 
+
     save(fields, files){
 
       return new Promise((resolve, reject)=>{
 
         fields.photo = `images/${path.parse(files.photo.path).base}`;
 
-        conn.query(`
-            INSERT INTO tb_menus (title, description, price, photo)
-            VALUES (?, ?, ?, ?)
-        `,[
-          fields.photo,
+        let query, queryPhoto = '', params = [
+          fields.title,
           fields.description,
-          fields.price,
-          fields.photo
-        ], (err, results)=>{
+          fields.price
+        ];
+
+        if (files.photo.name) {
+
+          queryPhoto = ',photo = ?'
+
+          params.push(fields.photo);
+
+        }
+
+        if(parseInt(fields.id) > 0) {
+
+          params.push(fields.id);
+
+          query =`
+            UPDATE tb_menus
+            SET title = ?,
+              descripton = ?,
+              price = ?,
+              photo = ?
+            WHERE id = ?
+          `;
+
+        } else {
+
+          if (!files.photo.name) {
+            reject('Envie a foto do prato.');
+          }
+
+          query = `
+          INSERT INTO tb_menus (title, description, price, photo)
+          VALUES (?, ?, ?, ?)
+          `;
+
+        }
+
+        conn.query(query ,params, (err, results)=>{
           
           if (err) {
             reject(err);
